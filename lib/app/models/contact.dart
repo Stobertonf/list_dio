db3679a37f77ce8fdb4c815ff7742a362ef3cf22
stobertonf <stobertonf@gmail.com>
2023-06-08 14:14:31 -0300

    feat(Contact-Page): Adding New Columns In The Model.

--- lib/app/models/contact.dart
+++ lib/app/models/contact.dart
@@ -2,10 +2,24 @@ class Contact {
   int id;
   String name;
   String phone;
+  String email;
+  String profession;
 
-  Contact({required this.id, required this.name, required this.phone});
+  Contact({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.profession,
+  });
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name, 'phone': phone};
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'email': email,
+      'profession': profession,
+    };
   }
 }
