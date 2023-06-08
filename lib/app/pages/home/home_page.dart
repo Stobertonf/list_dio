061950817dd6d25465fe5d550b868dd8da5cdb05
stobertonf <stobertonf@gmail.com>
2023-06-08 12:50:24 -0300

    feat(Home-Page): Creating The Home-Page File.

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,24 +1,149 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late final File _imageFile;
+  final _formKey = GlobalKey<FormState>();
+  final _nomeController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _telefoneController = TextEditingController();
+  final _profissaoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageFile = File('');
+  }
+
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(
+      source: ImageSource.gallery,
+    );
+    setState(() {
+      _imageFile = File(pickedFile!.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.blue,
-      child: const Text(
-        'Home Page',
-        style: TextStyle(
-          fontSize: 40,
-          fontFamily: 'Poppins',
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Cadastro',
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'Poppins',
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              GestureDetector(
+                onTap: _pickImage,
+                child: CircleAvatar(
+                  radius: 140,
+                  backgroundImage: _imageFile.path.isNotEmpty
+                      ? FileImage(_imageFile)
+                      : Image.asset('assets/images/icon_user.jpg').image,
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _nomeController,
+                decoration: const InputDecoration(
+                  labelText: 'Nome',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (_nomeController.text.isEmpty) {
+                    return 'Digite seu nome';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _telefoneController,
+                decoration: const InputDecoration(
+                  labelText: 'Telefone',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (_telefoneController.text.isEmpty) {
+                    return 'Digite seu telefone';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (_emailController.text.isEmpty) {
+                    return 'Digite seu e-mail';
+                  }
+                  if (_emailController.text.contains('@') != true) {
+                    return 'Digite um email válido';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _profissaoController,
+                decoration: const InputDecoration(
+                  labelText: 'Profissão',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (_profissaoController.text.isEmpty) {
+                    return 'Digite sua profissão';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  if (_formKey.currentState?.validate() != true) {
+                    return;
+                  }
+                },
+                child: const Text(
+                  'Salvar',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
